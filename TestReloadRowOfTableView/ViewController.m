f3592708c7a5e27ec3686c9370d60ab816bfa36b
jzh <jiangzenghui005@163.com>
2020-10-09 17:51:43 +0800

    UITableView 刷新测试

--- TestReloadRowOfTableView/ViewController.m
+++ TestReloadRowOfTableView/ViewController.m
@@ -7,7 +7,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 
 @end
 
@@ -15,8 +18,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
+- (IBAction)buttonAction:(id)sender {
+    NSInteger row = -1;
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:row inSection:0];
+    NSLog(@"todo --- %ld ---- %@", (long)row, indexPath);
+    [_tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellId = @"cellId";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"第 %ld 个", indexPath.row];
+    
+    return cell;
+}
 
 @end
